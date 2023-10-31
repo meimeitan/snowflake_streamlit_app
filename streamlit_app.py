3361c44cca33ed2a232363f71b774d079894aaa5
Mei Tan <58686831+meimeitan@users.noreply.github.com>
2023-10-31 19:04:55 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,6 @@ streamlit.write('The user entered', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Orange")
 
-
 ##normalise the json version of response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 ##output shows in table
