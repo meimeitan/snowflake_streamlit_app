e67fdbb6032bec38c370e5bde4d387ae1bc63ccf
Mei Tan <58686831+meimeitan@users.noreply.github.com>
2023-11-03 17:51:48 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ try:
 #streamlit.write('The user entered', fruit_choice)
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
-  else
+  else:
 ##new section to display fruityvice api response
 #import requests
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
