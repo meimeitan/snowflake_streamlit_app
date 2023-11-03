39f46b85c357164c66b9f7ff05cf04728467eb3e
Mei Tan <58686831+meimeitan@users.noreply.github.com>
2023-11-03 13:54:43 +0000

    Update streamlit_app.py
    Amend display fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ streamlit.write('The user entered', fruit_choice)
 
 ##new section to display fruityvice api response
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Orange")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 ##normalise the json version of response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
