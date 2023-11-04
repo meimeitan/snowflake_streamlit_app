1f3484d5918ce7a9923ddbf3c5545097961bc685
Mei Tan <58686831+meimeitan@users.noreply.github.com>
2023-11-04 15:39:35 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ if streamlit.button('Get Fruit Load List'):
 #add text entry box and send the input to snowflake connector as part of API call
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+         my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
          return "Thanks for adding " + new_fruit
         
 fruit_choice2 = streamlit.text_input('What fruit would you like add?')
